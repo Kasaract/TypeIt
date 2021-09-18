3c5a91f8b5e5f24fea0dace48ce2d4c31af100b2
Gary Nguyen <nguyeng@mit.edu>
2021-09-18 14:35:52 -0400

    Add dynamic routing for getting sample text

---
+++ backend/sampleText.js
@@ -0,0 +1,8 @@
+const sampleText = {
+  ZH: '你好。 我是一个打字程序，旨在帮助您学习如何打字。',
+  EN: 'This is a sample text in English. I am a typing program intended to help you learn how to type.',
+  TH: 'เป็นมนุษย์สุดประเสริฐเลิศคุณค่า กว่าบรรดาฝูงสัตว์เดรัจฉาน จงฝ่าฟันพัฒนาวิชาการ อย่าล้างผลาญฤๅเข่นฆ่าบีฑาใคร ไม่ถือโทษโกรธแช่งซัดฮึดฮัดด่า หัดอภัยเหมือนกีฬาอัชฌาสัย ปฏิบัติประพฤติกฎกำหนดใจ พูดจาให้จ๊ะๆ จ๋าๆ น่าฟังเอยฯ',
+  VI: 'Đây là một văn bản mẫu bằng tiếng Anh. Tôi là một chương trình gõ nhằm giúp bạn học cách gõ.',
+};
+
+module.exports = sampleText;
